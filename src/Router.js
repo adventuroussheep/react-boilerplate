9f370e4de0a71644691fee175a0b0391de545bb9
Jacob Haas <jacob_haas_7@hotmail.com>
2020-03-31 15:55:40 -0700

    Initial commit, Material-UI, API Hooks

---
+++ src/Router.js
@@ -0,0 +1,23 @@
+import React, { Component } from "react";
+import { BrowserRouter, Route, Switch } from "react-router-dom";
+import HomePage from './Components/HomePage/HomePage';
+
+class Router extends Component {
+  render() {
+    return (
+      <>
+        <BrowserRouter>
+          <Switch>
+              {/* Route for homepage in Components */}
+            <Route path="/" component={HomePage} />
+            
+              {/* Route for 404 page in Components */}
+            {/* <Route component={Page404} /> */}
+          </Switch>
+        </BrowserRouter>
+      </>
+    );
+  }
+}
+
+export default Router;
